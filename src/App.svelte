f6eac03055e8522e985c64f310d2114140093ca7
Anton Klock <klock.anton@gmail.com>
2023-05-23 14:55:58 +0200

    fixing some styling

--- src/App.svelte
+++ src/App.svelte
@@ -4,35 +4,17 @@
 </script>
 
 <main class="container">
-  <h1>Welcome to Tauri!</h1>
-
   <Login />
-
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-
-  <p>Click on the Tauri, Vite, and Svelte logos to learn more.</p>
-
-  <div class="row">
-    <Greet />
-  </div>
 </main>
 
 <style>
-  .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #747bff);
+  h1 {
+    margin-bottom: 0px;
   }
 
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00);
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
   }
 </style>
--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -15,25 +15,33 @@
 <div class="container">
   {#if authenticated == false}
     <h1>Login</h1>
-    <button class={"loginButton"} on:click={handleLogin}>Login</button>
-    <form>
+    <form class="form">
       <input type="text" name="username" placeholder="username" />
-      <input type="text" name="password" placeholder="password" />
+      <input type="password" name="password" placeholder="password" />
       <input type="text" name="otp" placeholder="otp" />
     </form>
+    <button class={"loginButton"} on:click={handleLogin}>Login</button>
   {/if}
   {#if authenticated == true}
     <h1>Logout</h1>
     <button on:click={handleLogout}>Logout</button>
     <p
-      style={"color: green; background: lightgreen; font-weight: 800; width: 20%"}
+      style={"font-size: 0.75rem; color: green; background: lightgreen; font-weight: 800; width: 20%; border-radius: 1rem"}
     >
-      Login successful!
+      Login Successful
     </p>
   {/if}
 </div>
 
 <style>
+  .form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .form input {
+    margin-bottom: 1rem;
+  }
   .loginButton {
     width: 20%;
   }
